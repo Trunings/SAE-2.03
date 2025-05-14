8387b529a8b8ed1b3d93bfefdf27197433d7e98d
Trunings <alexis.charpentier2@edu.univ-eiffel.fr>
2025-05-14 22:02:02 +0200

    Ajout de fichier

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>One piece</title>
+    <link rel="stylesheet" href="Page.css">
+    <link rel="stylesheet" href="Header.css">
+</head>
+<body>
+
+    <header role="header">
+
+        <nav class="menu" role="navigation">
+            <div class="inner">
+                <div class="m-left">
+                    <h1 class="logo">One piece</h1>
+                </div>
+                <div class="m-right">
+                    <a href="#" class="m-link">Accueil</a>
+                    <a href="#" class="m-link">Encyclopédie</a>
+                    <a href="#" class="m-link">Petit jeu</a>
+                    <a href="#" class="m-link">Contact</a>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    
+</body>
+</html>
